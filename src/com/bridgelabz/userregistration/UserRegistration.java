936aacfc00d2aa14f9bce3cb95dd119687177573
mk3709 <mk3709@srmist.edu.in>
2024-02-08 10:35:40 +0530

    LastName

--- src/com/bridgelabz/userregistration/UserRegistration.java
+++ src/com/bridgelabz/userregistration/UserRegistration.java
@@ -3,22 +3,36 @@ import java.util.regex.*;
 import  java.util.Scanner;
 
 public class UserRegistration {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the First Name");
-            String firstName = scanner.nextLine();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the First Name");
+        String firstName = scanner.nextLine();
 
-        if(isFirstName(firstName))
+        System.out.println("Enter the Last Name");
+        String lastName = scanner.nextLine();
+
+        if (isFirstName(firstName))
             System.out.println("Valid First Name");
-      else
-        {
+        else {
             System.out.println("Invalid first name.");
         }
-        }
+        if(isLastName(lastName))
+            System.out.println("Valid Last name");
+        else
+            System.out.println("invalid last name");
+
+
+}
         private static boolean isFirstName(String firstName)
         {
             String regex="^[A-Z][a-zA-Z]{2,}$";
             return Pattern.matches(regex,firstName);
+        }
+
+        private static boolean isLastName(String lastname)
+        {
+            String regex="^[A-Z][a-zA-Z]{2,}$";
+            return Pattern.matches(regex,lastname);
         }
 
 }
